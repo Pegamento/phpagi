5bddb0c90e4edc701c1081538286496b01cb7661
RobinPegamento <80826379+RobinPegamento@users.noreply.github.com>
2024-08-29 16:19:50 +0200

    Fixed new_AsteriskManager() assigning to non-existing member. Removed references to mysql_errno(), as that function has been removed since PHP 7.0.0. Commented out references to mysqli_errno(), because that function needs a database connection object passed to it for it to function.

--- src/phpagi.php
+++ src/phpagi.php
@@ -1562,10 +1562,10 @@ class AGI
      */
     function &new_AsteriskManager()
     {
-        $this->asm = new AGI_AsteriskManager(null, $this->config['asmanager']);
-        $this->asm->setPagi($this);
-        $this->config['asmanager'] =& $this->asm->config['asmanager'];
-        return $this->asm;
+        $this->asmanager = new AGI_AsteriskManager(null, $this->config['asmanager']);
+        $this->asmanager->setPagi($this);
+        $this->config['asmanager'] =& $this->asmanager->config['asmanager'];
+        return $this->asmanager;
     }
 
 
@@ -1786,14 +1786,13 @@ function phpagi_error_handler($level, $message, $file, $line, $context = '')
         $subject = "$basefile/$line/$level: $message";
         $message = "$level: $message in $file on line $line\n\n";
 
-        if(strpos(' '.strtolower($message), 'mysql')) {
-            if(function_exists('mysql_errno')) {
-                $message .= 'MySQL error ' . mysql_errno() . ": " . mysql_error() . "\n\n";
-            }
-            else if(function_exists('mysqli_errno')) {
-                $message .= 'MySQL error ' . mysqli_errno() . ": " . mysqli_error() . "\n\n";
-            }
-        }
+        // TODO: find a way to get a mysqli database context to pass to the mysqli_errno functions.
+        // if(strpos(' '.strtolower($message), 'mysql')) {
+        //     if(function_exists('mysqli_errno')) {
+        //         $db = null;
+        //         $message .= 'MySQL error ' . mysqli_errno($db) . ": " . mysqli_error($db) . "\n\n";
+        //     }
+        // }
 
         // figure out who we are
         if(function_exists('socket_create'))
